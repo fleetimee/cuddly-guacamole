a817020ab4e7007ba61f11319f8b931e8a14f95d
Novian Andika <novian.andika@students.amikom.ac.id>
2025-04-22 15:28:55 +0700

    feat: refactor App composable to use MainViewModel for greeting list
    Signed-off-by: Novian Andika <novian.andika@students.amikom.ac.id>

--- composeApp/src/androidMain/kotlin/org/fleetime/testwithios/App.kt
+++ composeApp/src/androidMain/kotlin/org/fleetime/testwithios/App.kt
@@ -1,5 +1,6 @@
 package org.fleetime.testwithios
 
+import MainViewModel
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -7,22 +8,22 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-@Preview
-fun App() {
+fun App(mainViewModel: MainViewModel = viewModel()) {
     MaterialTheme {
-        val greeting = remember { Greeting().greet() }
+        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
 
         Column(
             modifier = Modifier.padding(all = 20.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            greeting.forEach { greeting ->
+            greetings.forEach { greeting ->
                 Text(greeting)
                 Divider()
             }
